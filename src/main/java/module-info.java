b98dc0ccee40a5489df1f35dea4162bee79f5370
FerChikito <20243rd022@utez.edu.mx>
2025-07-15 21:50:57 -0600

    Primer commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.example.int_biblioteca {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.int_biblioteca to javafx.fxml;
+    exports org.example.int_biblioteca;
+}
