d6e6ae8d952f2d5504cc6843efb98c36e5461390
FerChikito <20243rd022@utez.edu.mx>
2025-07-16 19:51:41 -0600

    agregue la funcion del ojo y detalles al IdS

--- src/main/java/org/example/int_biblioteca/HelloController.java
+++ src/main/java/org/example/int_biblioteca/HelloController.java
@@ -1,14 +1,34 @@
 package org.example.int_biblioteca;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Button ojo;
+    @FXML
+    private PasswordField pass;
+    @FXML
+    private TextField verPass;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void verPass(ActionEvent event) {
+        String passNormal = pass.getText();
+
+        pass.setVisible(false);
+        verPass.setVisible(true);
+        verPass.setText(passNormal);
     }
 }
