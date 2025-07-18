305d87ed9fb947ebfaf0ae7150382e71f952ed32
FerChikito <20243rd022@utez.edu.mx>
2025-07-18 17:10:00 -0600

    Aguste EL hello-view y agregue mejor funcioonalidad en el hellocontroller

--- src/main/java/org/example/int_biblioteca/HelloController.java
+++ src/main/java/org/example/int_biblioteca/HelloController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
@@ -18,17 +19,30 @@ public class HelloController {
     @FXML
     private TextField verPass;
 
+    private Stage stage;
+
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 
     @FXML
+    private boolean isPasswordVisible = false; // Variable para controlar la visibilidad
+    @FXML
     public void verPass(ActionEvent event) {
         String passNormal = pass.getText();
-
-        pass.setVisible(false);
-        verPass.setVisible(true);
-        verPass.setText(passNormal);
+        if (!isPasswordVisible) {
+            // Mostrar la contraseña
+            pass.setVisible(false);
+            verPass.setVisible(true);
+            verPass.setText(passNormal);
+            isPasswordVisible = true; // Cambiar el estado a visible
+        } else {
+            // Ocultar la contraseña
+            pass.setVisible(true);
+            verPass.setVisible(false);
+            isPasswordVisible = false; // Cambiar el estado a no visible
+        }
     }
+
 }
