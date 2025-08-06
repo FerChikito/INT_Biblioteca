5a4267ce514de324f9851fb1b1e37dde4122e44f
Alexi <alexis_josh30@hotmail.com>
2025-08-05 21:40:03 -0600

    Se declararon todas las instancias necesarias.

--- src/main/java/org/example/RegistroController.java
+++ src/main/java/org/example/RegistroController.java
@@ -1,8 +1,18 @@
+package org.example;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class RegistroController {
 
-    @FXML
-    private TextField nombreField;
-}
+    @FXML private TextField nombreField;
+    @FXML private TextField apellidoField;
+    @FXML private DatePicker fechaNacimientoPicker;
+    @FXML private TextField correoField;
+    @FXML private TextField confirmarCorreoField;
+    @FXML private PasswordField passwordField;
+    @FXML private PasswordField confirmPasswordField;
+    @FXML private CheckBox terminosCheck;
+    @FXML private Button registrarBtn;
+    @FXML private Button togglePassBtn;
+    @FXML private Button toggleConfirmPassBtn;
